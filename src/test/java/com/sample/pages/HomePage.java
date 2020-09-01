2c52b332e11c1908b525d38b0175f8a4c863b0f7
snehabura <42755023+snehabura@users.noreply.github.com>
2020-09-01 12:32:14 -0600

    Code cleanup (#1)
    * code cleanup
    
    * writing some descriptions

--- src/test/java/com/sample/pages/HomePage.java
+++ src/test/java/com/sample/pages/HomePage.java
@@ -1,41 +1,41 @@
 package com.sample.pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-	
+
 	WebDriver driver;
-	
-	String raptorHomePage = "https://raptormaps.com/";
-	
-	@FindBy(id="hs-eu-decline-button")
+
+	@FindBy(id = "hs-eu-decline-button")
 	WebElement cookieAlert;
-	
-	@FindBy(id="menu-item-5086")
+
+	@FindBy(id = "menu-item-5086")
 	WebElement aboutLink;
-	
+
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
-		PageFactory.initElements(driver, this); // check why do we use this
+		PageFactory.initElements(driver, this); 
 	}
 
 	public boolean isCookieAlertDisplayed() {
-		return cookieAlert.isDisplayed();
+		try {
+			cookieAlert.isDisplayed();
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+		return true;
 	}
-	
+
 	public void declineCookieAlert() {
 		cookieAlert.click();
 	}
-	
+
 	public WebElement getAboutElement() {
 		return aboutLink;
 	}
-	
-	
-	
-
 
 }
